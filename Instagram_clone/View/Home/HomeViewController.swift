869a00561807d42938295a5550b7207c34bd2486
kamimi01 <awakaru0122@gmail.com>
2019-12-15 18:36:27 +0900

    ファイル構成を変更

---
+++ Instagram_clone/View/Home/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  Instagram_clone
+//
+//  Created by Mika Urakawa on 2019/12/15.
+//  Copyright © 2019 kamimi01. All rights reserved.
+//
+
+import UIKit
+import ESTabBarController
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpTab()
+    }
+    
+    func setUpTab() {
+        // 画像ファイル名を指定してタブを表示する
+        let tabBarController: ESTabBarController! 
+    }
+
+}
