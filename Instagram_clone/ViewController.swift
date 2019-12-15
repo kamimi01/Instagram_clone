a728d308467fda8cd1e0c825479900d62588214b
kamimi01 <awakaru0122@gmail.com>
2019-12-15 17:39:29 +0900

    Initial Commit

---
+++ Instagram_clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_clone
+//
+//  Created by Mika Urakawa on 2019/12/15.
+//  Copyright © 2019 kamimi01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
