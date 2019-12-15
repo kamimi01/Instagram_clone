ef03190e2536a329d25d2a284b0906953fa6960b
kamimi01 <awakaru0122@gmail.com>
2019-12-15 17:58:37 +0900

    First commit

---
+++ Instagram_clone/New Group/View/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Instagram_clone
+//
+//  Created by Mika Urakawa on 2019/12/15.
+//  Copyright © 2019 kamimi01. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram_clone/New Group/View/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginInitialViewController.swift
+//  Instagram_clone
+//
+//  Created by Mika Urakawa on 2019/12/15.
+//  Copyright © 2019 kamimi01. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
